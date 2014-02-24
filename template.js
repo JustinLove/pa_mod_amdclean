edd14184e7f29dab2efa66fc3f407d6d3f1fa185
Justin Love <git@JustinLove.name>
2014-02-24 13:43:13 -0600

    include some information in the after notes

--- template.js
+++ template.js
@@ -7,8 +7,12 @@ exports.description = 'Create a minimal mod for Planitary Annihilation';
 //exports.notes = ''
 
 // Template-specific notes to be displayed after question prompts.
-//exports.after = '' +
-  //'';
+exports.after = "Note: you may want to work on a copy of modinfo.json and copy it back if you're using PAMM.  PAMM rewrites the file without formmating every time you toggle enabled." +
+  "\n\n" +
+  "For further modding info, check out the forums." +
+  "\n\n" +
+  "https://forums.uberent.com/threads/pa-modding-reference-guides-applications-tools.48136/" +
+  "https://forums.uberent.com/threads/guide-getting-your-mod-on-pamm.55189/";
 
 // Any existing file or directory matching this wildcard will cause a warning.
 exports.warnOn = '*';
